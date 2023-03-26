e7794c3b37142e4c41685f4569bba54a90fbf2ab
Enzo Rodrigues Gracindo <129012135+Enzocks0820@users.noreply.github.com>
2023-03-26 20:04:39 -0300

    Add files via upload

---
+++ Enzao/Enzao/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Enzao
+{
+    public partial class Form1 : Form
+    {
+     string connectionString = "datasource=mysql08-farm36.kinghost.net;port=3306;username=cursoinvertido03;password=exercicio123;database=cursoinvertido03;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            // Your query,
+            string query = "SELECT * FROM user";
+
+            // Prepare the connection
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+            listUsers();
+           
+
+            // Let's do it !
+            try
+            {
+                // Open the database
+                databaseConnection.Open();
+
+                // Execute the query
+                reader = commandDatabase.ExecuteReader();
+
+                // All succesfully executed, now do something
+
+                // IMPORTANT : 
+                // If your query returns result, use the following processor :
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        // As our database, the array will contain : ID 0, FIRST_NAME 1,LAST_NAME 2, ADDRESS 3
+                        // Do something with every received database ROW
+                        string[] row = { reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3) };
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nenhum dado encontrado.");
+                }
+
+                // Finally close the connection
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Show any error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listUsers();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SaveUser();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            listUsers();
+
+        }
+
+        private void SaveUser()
+        {
+          
+            string query = "INSERT INTO user(`id`, `first_name`, `last_name`, `address`) VALUES (NULL, '" + textBox1.Text + "', '" + textBox2.Text + "', '" + textBox3.Text + "')";
+            // Which could be translated manually to :
+            // INSERT INTO user(`id`, `first_name`, `last_name`, `address`) VALUES (NULL, 'Bruce', 'Wayne', 'Wayne Manor')
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+
+            try
+            {
+                databaseConnection.Open();
+                MySqlDataReader myReader = commandDatabase.ExecuteReader();
+
+                MessageBox.Show("Usuário inserido com sucesso");
+
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Show any error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void listUsers()
+        {
+            // Select all
+            string query = "SELECT * FROM user";
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+                // Success, now list 
+
+                // If there are available rows
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader.GetString(0) + " - " + reader.GetString(1) + " - " + reader.GetString(2) + " - " + reader.GetString(3));
+                        // Example to save in the listView1 :
+                        string[] row = { reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3) };
+                        var listViewItem = new ListViewItem(row);
+                        listView1.Items.Add(listViewItem);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("NADA FOI ENCONTRADO");
+                }
+
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void updateUser()
+        {
+            // Update the properties of the row with ID 1
+            string query = "UPDATE `user` SET `first_name`='Willy',`last_name`='Wonka',`address`='Chocolate factory' WHERE id = 1";
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+
+                // Succesfully updated
+
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Ops, maybe the id doesn't exists ?
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DeleteUser_Click(object sender, EventArgs e)
+        {
+            
+            string query = "DELETE FROM `user` WHERE id = 1 ";
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+
+             
+
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           // atualizarUsuarios();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form3 visivel = new Form3();
+            visivel.ShowDialog();
+
+        }
+    }
+}
---
+++ Enzao/Enzao/Form3.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+
+namespace Enzao
+{
+    public partial class Form3 : Form
+    {
+        string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=teste;";
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM admin WHERE usuario = '" + textBox1.Text + "' and senha = '" + textBox2.Text + "' ";
+
+
+
+
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+
+
+
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+
+
+
+            commandDatabase.CommandTimeout = 60;
+
+
+
+            MySqlDataReader reader;
+
+
+
+
+
+
+
+            try
+
+
+
+            {
+
+
+
+                // Open the database 
+
+
+
+                databaseConnection.Open();
+
+
+
+                reader = commandDatabase.ExecuteReader();
+
+
+
+
+
+
+
+                if (reader.HasRows)
+
+
+
+                {
+
+                    Form4 visivel = new Form4();
+
+                    visivel.ShowDialog();
+
+                    Application.ExitThread();
+
+
+
+
+
+                }
+
+
+
+                else
+
+
+
+                {
+
+                    MessageBox.Show("Usuario ou Senha incorretos", "Falha no login", MessageBoxButtons.OK);
+
+
+
+                }
+
+
+
+                // Finally close the connection 
+
+
+
+                databaseConnection.Close();
+
+
+
+            }
+
+
+
+            catch (Exception ex)
+
+
+
+            {
+
+
+
+                // Show any error message. 
+
+
+
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK);
+
+
+
+            }
+        }
+    }
+}
